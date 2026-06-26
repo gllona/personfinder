bfe7a8458a5391319861b10a2a993c71f203d7f4
Gorka Llona <gllona@gmail.com>
2026-06-26 21:38:02 +0200

    implement persistent storage for docker compose configuration

--- docker/gae-run-app.sh
+++ docker/gae-run-app.sh
@@ -6,7 +6,18 @@ API_PORT=49532
 
 IP_ADDR=`ip addr list eth0 | grep 'inet ' | cut -d' ' -f6 | cut -d'/' -f1`
 
+DATA_DIR=${PERSONFINDER_DIR}data
+
 cd ${PERSONFINDER_DIR}
 echo "Starting Person Finder server"
-dev_appserver.py app --host ${IP_ADDR} --port ${PORT} --admin_host=${IP_ADDR} --admin_port=${HOST_PORT} --api_host=${IP_ADDR} --api_port=${API_PORT} --skip_sdk_update_check
+dev_appserver.py app \
+  --host ${IP_ADDR} \
+  --port ${PORT} \
+  --admin_host=${IP_ADDR} \
+  --admin_port=${HOST_PORT} \
+  --api_host=${IP_ADDR} \
+  --api_port=${API_PORT} \
+  --datastore_path=${DATA_DIR}/datastore.db \
+  --blobstore_path=${DATA_DIR}/blobstore \
+  --skip_sdk_update_check
 echo "Person Finder server stopped"
